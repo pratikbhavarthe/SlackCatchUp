65640ff253084c9d5d2b8aeb638b034666e4d2e8
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-01-17 17:56:09 +0530

    installed few dependencies along with types.ts file

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import { ButtonProps } from '../types';
