a8b53afcbc5d0045a56975c63266b8d9119d9814
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-01-17 20:27:40 +0530

    added animated thread actions component

---
+++ components/ThreadActions.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+import Animated, { SlideInDown, SlideOutDown } from 'react-native-reanimated';
+import Button from './Button';
+import { ThreadActionsProps } from '../types';
+import React from "react";
+
+const ThreadActions: React.FC<ThreadActionsProps> = ({
+  onMarkRead,
+  onMarkUnread,
+}) => {
+  return (
+    <Animated.View
+      style={styles.cardActionsContainer}
+      entering={SlideInDown.springify().mass(0.5).damping(15).delay(250)}
+      exiting={SlideOutDown.springify().mass(0.5).damping(15)}
+    >
+      <Button title='Keep Unread' variant='secondary' onPress={onMarkUnread} />
+      <Button title=' Mark as Read' variant='primary' onPress={onMarkRead} />
+    </Animated.View>
+  );
+};
+
+export default ThreadActions;
+
+const styles = StyleSheet.create ({
+    cardActionsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        columnGap: 10,
+    },
+});
